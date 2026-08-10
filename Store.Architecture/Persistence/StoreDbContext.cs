17f3ba7a41c9be97f5014feb93ce6b4ead0d3d90
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-10 15:42:06 +0300

    Implement StoreDbContext

---
+++ Store.Architecture/Persistence/StoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Domain.Entities;
+
+namespace Store.Infrastructure.Persistence
+{
+    public class StoreDbContext : DbContext
+    {
+
+        public StoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Invoice> Invoices => Set<Invoice>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<Supplier> Suppliers => Set<Supplier>();
+    }
+}
