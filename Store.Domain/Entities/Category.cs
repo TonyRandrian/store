1b559af941b606e40f8ab72c798da2c288c2d694
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-10 12:32:02 +0300

    Implement Category entity

---
+++ Store.Domain/Entities/Category.cs
@@ -0,0 +1,38 @@
+﻿namespace Store.Domain.Entities
+{
+    internal class Category
+    {
+
+        public Category(int id, string name, Category? parentCategory)
+        {
+            Id = id;
+            Name = name;
+            ParentCategory = parentCategory;
+        }
+
+        public int Id
+        {
+            get;
+
+            set;
+        }
+
+        public string Name
+        {
+            get;
+
+            set
+            {
+                ArgumentException.ThrowIfNullOrWhiteSpace(value, "Can't create a product with a null or empty name")
+                field = value;
+            }
+        }
+
+        public Category? ParentCategory
+        {
+            get;
+
+            set;
+        }
+    }
+}
