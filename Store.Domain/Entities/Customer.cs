f085ee81fc1edef11de5dbacfb300f5dd997e568
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-10 14:09:40 +0300

    Create Customer class

---
+++ Store.Domain/Entities/Customer.cs
@@ -0,0 +1,30 @@
+﻿namespace Store.Domain.Entities
+{
+    internal class Customer
+    {
+
+        public Customer(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id
+        {
+            get;
+
+            set;
+        }
+
+        public string Name
+        {
+            get;
+
+            set
+            {
+                ArgumentException.ThrowIfNullOrWhiteSpace(value, "Cannot create a customer with a null or empty name");
+                field = value;
+            }
+        }
+    }
+}
