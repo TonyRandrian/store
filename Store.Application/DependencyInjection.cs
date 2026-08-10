ad72cdeffbb49760ed22abc8204e44d3663cb6ac
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-10 16:05:18 +0300

    Create dependencies injection

---
+++ Store.Application/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Store.Application
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddApplicationDI(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ Store.Domain/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Store.Domain
+{
+    public static class DependencyInjection
+    {
+
+        public static IServiceCollection AddDomainDI(IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
