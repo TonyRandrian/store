e405c20448a96ad82aa1797aa0c5d5f97cf8fcc8
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-10 14:01:46 +0300

    Create Product class

---
+++ Store.Domain/Entities/Product.cs
@@ -0,0 +1,49 @@
+﻿namespace Store.Domain.Entities
+{
+    internal class Product
+    {
+
+        public Product(int id, string name, Category? category)
+        {
+            Id = id;
+            Name = name;
+            Category = category;
+        }
+
+        public int Id
+        {
+            get;
+
+            set;
+        }
+
+        public string Name
+        {
+            get;
+
+            set
+            {
+                ArgumentNullException.ThrowIfNullOrEmpty(value, "Cannot create a product with a null or empty name");
+                field = value;
+            }
+        }
+
+        public Category? Category
+        {
+            get;
+
+            set;
+        }
+
+        public decimal Price
+        {
+            get;
+
+            set
+            {
+                ArgumentOutOfRangeException.ThrowIfLessThan(value, 0, "Product's price should not be negative");
+                field = value;
+            }
+        }
+    }
+}
