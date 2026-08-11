dedb3df8b1de027a211ea1b3c51f00f9901d3176
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-11 11:38:07 +0300

    Implement CustomerRepository
    Update ICustomerRepository to have public methods and implement all the methods in CustomerRepository

---
+++ Store.Application/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,13 @@
+﻿using Store.Domain.Entities;
+
+namespace Store.Application.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        public Task<List<Customer>> AGetAll();
+        public Task<Customer?> AGetById(int id);
+        public Task AAdd(Customer customer);
+        public Task AUpdate(Customer customer);
+        public Task ADelete(int id);
+    }
+}
---
+++ Store.Architecture/Repositories/CustomerRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Application.Interfaces;
+using Store.Domain.Entities;
+using Store.Infrastructure.Persistence;
+
+namespace Store.Infrastructure.Repositories
+{
+    public class CustomerRepository(StoreDbContext context) : ICustomerRepository
+    {
+        private readonly StoreDbContext Context = context;
+
+
+        public async Task<List<Customer>> AGetAll()
+        {
+            return await Context.Customers.ToListAsync();
+        }
+
+        public async Task<Customer?> AGetById(int id)
+        {
+            return await Context.Customers.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task AAdd(Customer customer)
+        {
+            Context.Customers.Add(customer);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task AUpdate(Customer customer)
+        {
+            Context.Customers.Update(customer);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task ADelete(int id)
+        {
+            Customer? customer = await AGetById(id);
+
+            if (customer == null)
+                return;
+
+            Context.Customers.Remove(customer);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
