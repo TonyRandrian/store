b4b04eb4049e5d59c4fe4a5e9348b16d9e57683d
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-11 12:02:02 +0300

    Implement InvoiceRepository
    Update IInvoiceRepository to make all methods public and implement them in InvoiceRepository

---
+++ Store.Application/Interfaces/IInvoiceRepository.cs
@@ -0,0 +1,13 @@
+﻿using Store.Domain.Entities;
+
+namespace Store.Application.Interfaces
+{
+    public interface IInvoiceRepository
+    {
+        public Task<List<Invoice>> AGetAll();
+        public Task<Invoice?> AGetById(int id);
+        public Task AAdd(Invoice invoice);
+        public Task AUpdate(Invoice invoice);
+        public Task ADelete(int id);
+    }
+}
---
+++ Store.Architecture/Repositories/InvoiceRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Application.Interfaces;
+using Store.Domain.Entities;
+using Store.Infrastructure.Persistence;
+
+namespace Store.Infrastructure.Repositories
+{
+    public class InvoiceRepository(StoreDbContext context) : IInvoiceRepository
+    {
+        private readonly StoreDbContext Context = context;
+
+            
+        public async Task<List<Invoice>> AGetAll()
+        {
+            return await Context.Invoices.ToListAsync();
+        }
+
+        public async Task<Invoice?> AGetById(int id)
+        {
+            return await Context.Invoices.FirstOrDefaultAsync(i => i.Id == id);
+        }
+
+        public async Task AAdd(Invoice invoice)
+        {
+            Context.Invoices.Add(invoice);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task AUpdate(Invoice invoice)
+        {
+            Context.Invoices.Update(invoice);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task ADelete(int id)
+        {
+            Invoice? invoice = await AGetById(id);
+
+            if (invoice == null)
+                return;
+
+            Context.Invoices.Remove(invoice);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
