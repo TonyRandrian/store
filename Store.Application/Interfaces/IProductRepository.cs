76a0c92636f897b2d1bf60968530cd503447e294
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-11 11:27:52 +0300

    Implement ProductRepository
    Make all IProductRepository methods public and implements then in ProductRepository

--- Store.Application/Interfaces/IProductRepository.cs
+++ Store.Application/Interfaces/IProductRepository.cs
@@ -4,10 +4,10 @@ namespace Store.Application.Interfaces
 {
     public interface IProductRepository
     {
-        Task<List<Product>> AGetAll();
-        Task<Product> AGetById(int id);
-        Task AAdd(Product product);
-        Task AUpdate(Product product);
-        Task ADelete(int id);
+        public Task<List<Product>> AGetAll();
+        public Task<Product?> AGetById(int id);
+        public Task AAdd(Product product);
+        public Task AUpdate(Product product);
+        public Task ADelete(int id);
     }
 }
---
+++ Store.Architecture/Repositories/ProductRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Application.Interfaces;
+using Store.Domain.Entities;
+using Store.Infrastructure.Persistence;
+
+namespace Store.Infrastructure.Repositories
+{
+    public class ProductRepository(StoreDbContext context) : IProductRepository
+    {
+        private readonly StoreDbContext Context = context;
+
+
+        public async Task<List<Product>> AGetAll()
+        {
+            return await Context.Products.ToListAsync();
+        }
+
+        public async Task<Product?> AGetById(int id)
+        {
+            return await Context.Products.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task AAdd(Product product)
+        {
+            await Context.Products.AddAsync(product);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task AUpdate(Product product)
+        {
+            Context.Products.Update(product);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task ADelete(int id)
+        {
+            Product? product = await Context.Products.FirstOrDefaultAsync(p => p.Id == id);
+
+            if (product == null)
+                return;
+
+            Context.Products.Remove(product);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
