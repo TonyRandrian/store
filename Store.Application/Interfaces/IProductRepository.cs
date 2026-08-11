5d60d74ed6ea30f192a86a417476add8a4fed18e
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-11 08:49:07 +0300

    Implement IProductRepository
    Add methods: getAll, getById, add, update, delete async

---
+++ Store.Application/Interfaces/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using Store.Domain.Entities;
+
+namespace Store.Application.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> AGetAll();
+        Task<Product> AGetById(int id);
+        Task AAdd(Product product);
+
+        Task AUpdate(Product product);
+
+        Task ADelete(int id);
+    }
+}
