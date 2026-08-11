34eaf7fba48b5fe08edc77bbe03dc273e030cd3c
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-11 12:09:31 +0300

    Implement SupplierRepository
    Update ISupplierRepository to make all methods public and implemnt them in SupplierRepository

---
+++ Store.Application/Interfaces/ISupplierRepository.cs
@@ -0,0 +1,13 @@
+﻿using Store.Domain.Entities;
+
+namespace Store.Application.Interfaces
+{
+    public interface ISupplierRepository
+    {
+        public Task<List<Supplier>> AGetAll();
+        public Task<Supplier?> AGetById(int id);
+        public Task AAdd(Supplier supplier);
+        public Task AUpdate(Supplier supplier);
+        public Task ADelete(int id);
+    }
+}
---
+++ Store.Architecture/Repositories/SupplierRepository.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Application.Interfaces;
+using Store.Domain.Entities;
+using Store.Infrastructure.Persistence;
+
+namespace Store.Infrastructure.Repositories
+{
+    public class SupplierRepository(StoreDbContext store) : ISupplierRepository
+    {
+        private readonly StoreDbContext Context = store;
+        
+
+        public async Task<List<Supplier>> AGetAll()
+        {
+            return await Context.Suppliers.ToListAsync();
+        }
+
+        public async Task<Supplier?> AGetById(int id)
+        {
+            return await Context.Suppliers.FirstOrDefaultAsync(s => s.Id == id);
+        }
+
+        public async Task AAdd(Supplier supplier)
+        {
+            Context.Suppliers.Add(supplier);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task AUpdate(Supplier supplier)
+        {
+            Context.Suppliers.Update(supplier);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task ADelete(int id)
+        {
+            Supplier? supplier = await AGetById(id);
+
+            if (supplier == null)
+                return;
+
+            Context.Suppliers.Remove(supplier);
+            await Context.SaveChangesAsync();
+        }
+    }
+}
