b1696dcb61f4781e22fbda2355ff374eaf84da49
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-25 11:29:19 +0300

    (feat): add fluent validator

---
+++ Store.Application/Features/Categories/Validators/CreateCategoryCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using Store.Application.Features.Categories.Commands.CreateCategory;
+using Store.Application.Features.Products.Commands.CreateProduct;
+
+namespace Store.Application.Features.Categories.Validators
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommand>
+    {
+
+        public CreateCategoryCommandValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .MaximumLength(200);
+        }
+    }
+}
