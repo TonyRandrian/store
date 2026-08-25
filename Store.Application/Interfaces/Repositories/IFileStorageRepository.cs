9e0598a60f320c16970a10e54be2257661731bf3
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-25 09:50:14 +0300

    (fix): remove unused classes

--- Store.Application/Interfaces/Repositories/IFileStorageRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Store.Application.Interfaces.Repositories
-{
-    public interface IFileStorageRepository
-    {
-        Task<string> SaveAsync(string fileStream, string fileName, string folder);
-        Task DeleteAsync(string path);
-    }
-}
