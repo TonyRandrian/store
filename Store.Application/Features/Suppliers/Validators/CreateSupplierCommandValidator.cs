5abcff8ce7c89dc590045ac206d030e0a48f9231
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-27 13:57:12 +0300

    (feat): supplier FluentValidator

---
+++ Store.Application/Features/Suppliers/Validators/CreateSupplierCommandValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+using Store.Application.Features.Suppliers.Commands.CreateSupplier;
+
+namespace Store.Application.Features.Suppliers.Validators
+{
+    public class CreateSupplierCommandValidator
+        : AbstractValidator<CreateSupplierCommand>
+    {
+
+        public CreateSupplierCommandValidator() { }
+    }
+}
---
+++ Store.Application/Features/Suppliers/Validators/DeleteSupplierCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Store.Application.Features.Suppliers.Commands.DeleteSupplier;
+using Store.Application.Interfaces.Repositories;
+
+namespace Store.Application.Features.Suppliers.Validators
+{
+    public class DeleteSupplierCommandValidator
+        : AbstractValidator<DeleteSupplierCommand>
+    {
+        private readonly ISupplierRepository _supplierRepository;
+
+
+        public DeleteSupplierCommandValidator(ISupplierRepository supplierRepository)
+        {
+            _supplierRepository = supplierRepository;
+
+            RuleFor(s => s.Id)
+                .MustAsync(async (id, cancellationToken) => await _supplierRepository.GetByIdAsync(id) != null)
+                .WithMessage(s => $"No supplier with the id {s.Id} found");
+        }
+    }
+}
