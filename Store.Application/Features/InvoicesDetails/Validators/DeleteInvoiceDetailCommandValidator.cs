f291272d61a35e4351eb0a04d0a4abbfd6c9c590
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-08-28 10:36:36 +0300

    (fix)

--- Store.Application/Features/InvoicesDetails/Validators/DeleteInvoiceDetailCommandValidator.cs
+++ Store.Application/Features/InvoicesDetails/Validators/DeleteInvoiceDetailCommandValidator.cs
@@ -1,11 +1,11 @@
 ﻿using FluentValidation;
-using Store.Application.Features.Invoices.Commands.DeleteInvoice;
+using Store.Application.Features.InvoicesDetails.Commands.DeleteInvoiceDetail;
 using Store.Application.Interfaces.Repositories;
 
 namespace Store.Application.Features.InvoicesDetails.Validators
 {
     public class DeleteInvoiceDetailCommandValidator
-        : AbstractValidator<DeleteInvoiceCommand>
+        : AbstractValidator<DeleteInvoiceDetailCommand>
     {
         private readonly IInvoiceDetailsRepository _invoiceDetailRepository;
 
@@ -16,7 +16,7 @@ namespace Store.Application.Features.InvoicesDetails.Validators
 
             RuleFor(i => i.Id)
                 .MustAsync(async (id, cancellationToken) => await _invoiceDetailRepository.GetByIdAsync(id) != null)
-                .WithMessage(i => $"No invoice detail with the id {i.Id} found ddd");
+                .WithMessage(i => $"No invoice detail with the id {i.Id} found");
         }
     }
 }
